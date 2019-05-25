b6268bf08aedc8eb9a8efd2b46afe68dce72a24e
WANG. YG <astwyg@126.com>
2019-05-25 22:36:07 +0800

    add tushare_token

--- server/stocks.py
+++ server/stocks.py
@@ -1,7 +1,9 @@
 import tushare as ts
+from conf.conf import get_conf
 
 
 def get_stocks():
+    ts.set_token(get_conf("tushare_token"))
     pro = ts.pro_api()
     data = pro.query('stock_basic', exchange='', list_status='L', fields='ts_code,symbol,name')
 
