b57bb65bc08eac1e0c6d0b364e233e7564562004
WANG. YG <astwyg@126.com>
2019-05-26 13:26:39 +0800

    在linux生产环境换PhantomJS浏览器

--- server/stocks.py
+++ server/stocks.py
@@ -1,11 +1,14 @@
 import tushare as ts
 from conf.conf import get_conf
 
+from conf.log import server_logger
 
 def get_stocks():
     ts.set_token(get_conf("tushare_token"))
     pro = ts.pro_api()
     data = pro.query('stock_basic', exchange='', list_status='L', fields='ts_code,symbol,name')
+
+    server_logger.debug("stock data fetched {}".format(len(data)))
 
     stocks = []
     for _, row in data.iterrows():
